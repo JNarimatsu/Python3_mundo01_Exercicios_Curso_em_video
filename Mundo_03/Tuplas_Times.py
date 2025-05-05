a256427828de1aae90b2cd4e8725aad769051c68
JNarimatsu <julianadiquental@gmail.com>
2025-05-05 14:59:21 -0300

    Mundo_03 primeiros exercicios

---
+++ Mundo_03/Tuplas_Times.py
@@ -0,0 +1,47 @@
+#Tuplas de times de futebol
+times = ('Flamengo', 'Palmeiras', 'São Paulo', 'Santos', 'Corinthians', 'Vasco da Gama', 'Atlético Mineiro', 'Fluminense', 
+        'Botafogo', 'Internacional', 'Grêmio', 'Cruzeiro', 'Athletico Paranaense', 'Bahia', 'Sport', 'Ceará', 'Fortaleza', 
+        'Goiás', 'Coritiba', 'Chapecoense')
+print('-='*15)
+print(f'Lista de times: {times}')
+print('-='*15)
+print(f'Os cinco primerios colocados são: {times[:5]}')
+print('-='*15)
+print(f'Os quatro últimos colocados são: {times[-4:]}')
+print('-='*15)
+print(f'Os times organizados em ordem alfabética: {sorted(times)}')
+print('-='*15)
+print(f'O Chapecoense está na {times.index("Chapecoense")+1} posição')
+print('-='*15)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
